3c38ae67c2634c6d51ab1d7e2c24c03222c74197
Maxwell Ochieng <maxwellmaxwels@gmail.com>
2023-11-07 13:51:20 +0300

    my second change

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,6 +3,15 @@ def add_two_numbers(a, b):
 
 # Example usage
 num1 = 1000
-num2 = 10000
+num2 = 1000
 result = add_two_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is {result}.")
+
+def divide_two_numbers(b,c):
+    return b*c
+print(divide_two_numbers(3,4))
+
+def subtruct_two_numbers(b,c):
+    return b*c
+print(subtruct_two_numbers(3,4))
+
