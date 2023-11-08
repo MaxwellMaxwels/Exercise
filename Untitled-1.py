d2eaf604bb46709b152fb0d57e6f759896fc5633
Maxwell Ochieng <maxwellmaxwels@gmail.com>
2023-11-08 14:54:53 +0300

    Teaching git

--- Untitled-1.py
+++ Untitled-1.py
@@ -8,10 +8,12 @@ result = add_two_numbers(num1, num2)
 print(f"The sum of {num1} and {num2} is {result}.")
 
 def divide_two_numbers(b,c):
-    return b*c
+    return b/c
 print(divide_two_numbers(3,4))
 
 def subtruct_two_numbers(b,c):
-    return b*c
+    return b-c
 print(subtruct_two_numbers(3,4))
 
+def division_of_two_numbers(b,c):
+    return b/c
